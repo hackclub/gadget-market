e55465706689b53a56ee7f335bbad24cedca4e49
acon <101356372+acornitum@users.noreply.github.com>
2026-06-09 14:04:05 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+
+
+<div class="page-outline">
+
+    <img src="/landingtop.svg" alt="" class="w-full object-cover object-center h-12 md:h-auto"/>
+
+    <div class="flex flex-col items-center justify-center">
+
+        <div class="my-10 mx-5 bg-[#F1EFE2] border-18 border-[#ACD43C] py-4 px-10 max-h-xl max-w-2xl -rotate-1 shadow relative hover:scale-101 transition">
+            <img src="/bluepin.svg" alt="" class="absolute left-1/2 -translate-x-1/2 -top-6"/>
+            <p class="text-[#EC7B00] heavywei text-3xl">welcome to gadget market!</p>
+            <div class="border-t-6 border-dotted border-[#ACD43C]"> </div>
+
+            <div class="space-y-6 text-center py-6 text-lg">
+
+                <p>This is a place for teens around the world to trade PCB’s!
+                Join <a href="https://hackclub.enterprise.slack.com/archives/C0AJQ0HBC12" class="text-[#4F74ED] link">#gadget-market</a> in Hack Club Slack for updates + questions! </p>
+
+                <p>Want to list your own PCB on the market? Check out [instructions], 
+                    then submit it! When your PCB gets approved, 
+                    you’ll get a 30 usd grant card to pay for shipping for your trades :D </p>
+
+                <p>You can also earn prizes when you do a certain amount of trades! Check out [instructions] for prizes. Have fun!</p>
+
+            </div>
+
+            <p class="text-right text-[#EC7B00] heavywei text-3xl">from hack club</p>
+        </div>
+
+    </div>
+
+
+</div>
+
