2f5061032512240e242cdfa0e15217bb7cd0c663
acon <101356372+acornitum@users.noreply.github.com>
2026-06-10 16:25:33 -0400

    styling changes

--- src/routes/listings/[id]/+page.svelte
+++ src/routes/listings/[id]/+page.svelte
@@ -3,8 +3,36 @@
     let listing = $derived(data.listing);
 </script>
 
-<div>
+<div class="page-outline p-5">
     <a href="/"><p>back</p></a>
-    <h1>{listing.project_name}</h1>
-    <img src="{listing.project_image}" alt="{listing.project_name}" />
+
+
+    <div class="flex space-x-8">
+
+        
+        <img src="{listing.project_image}" alt="{listing.project_name}" class="max-w-[40%]"/>
+        <div class="flex flex-col space-y-2">
+            <div class="flex flex-row space-x-2">
+                <a href="/" class="bg-blue-300 p-2">message on slack</a>
+                <a href="/" class="bg-blue-300 p-2">github repo</a>
+                <a href="/" class="bg-blue-300 p-2">video demo</a>
+            </div>
+
+            <div class="bg-[#F9F7EC] p-10 max-w-4xl">
+                <h1>{listing.project_name}</h1>
+                <p>{listing.project_tagline}</p> 
+                <p>{listing.project_description}</p>
+                <p>{listing.project_stock} in stock</p>
+                <p>assembled? {listing.project_assembled}</p>
+            </div>
+        </div>
+
+        
+
+    </div>
+
+
+
+
+    
 </div>
